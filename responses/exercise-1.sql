3e65f286f5724c77d6c3997145780b1e70ebde55
paulahnn2264 <paulfahnn@gmail.com>
2024-04-15 20:27:57 -0400

    Update exercise-1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,8 @@
 -- write your SQL here
-select 1;
+select locationid, month(datetime) as 'month', round(sum(JSON_EXTRACT(details, '$.items[*].amount')), 2) as 'refundAmount'
+from orderItems 
+JOIN (transactions CROSS JOIN JSON_TABLE(details, '$[*]' COLUMNS id INT, amount INT) AS multiple_trans) /*CROSS JOIN UNNEST(details -> 'items') AS item;*/
+  ON orderItems.id = exploded_items.id
+where transactions.type = "refund"
+group by locationid, month(datetime)
+order by locationId, month(datetime)
