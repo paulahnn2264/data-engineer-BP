937c50b4efde45c81a40b3ffadc02e702cbc6ec4
paulahnn2264 <paulfahnn@gmail.com>
2024-04-15 22:47:36 -0400

    Update exercise-1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -6,3 +6,7 @@ JOIN (transactions CROSS JOIN JSON_TABLE(details, '$[*]' COLUMNS id INT, amount 
 where transactions.type = "refund"
 group by locationid, month(datetime)
 order by locationId, month(datetime)
+
+--in the interest of time, i was able to figure out the concept and i'm sure there's some weird syntax thing,
+--but i wasn't able to get 1 part of it to compile for a final check, but I listed two possible ways to do it using mysql or postgress
+--hope that helps
